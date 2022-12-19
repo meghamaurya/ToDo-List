158ea660b16ba02f0fcdce544f4711df0c0c6653
megha <meghamini40@gmail.com>
2022-12-19 09:58:58 +0530

    added enter key event

--- src/Components/Todos/index.js
+++ src/Components/Todos/index.js
@@ -26,6 +26,18 @@ export default function Todos() {
             setIsCompleted(false);
         }
     }
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            if (todo.length === 0) {
+                setErr(true);
+            } else {
+                setErr(false)
+                dispatch(addTodo(uuidv4(), todo, isCompleted));
+                setTodo('');
+                setIsCompleted(false);
+            }
+        }
+    }
     const handleDeleteTodo = (id) => {
         const dltItem = todos.filter((data) => data.id !== id);
         dispatch(deleteTodo(dltItem));
@@ -50,8 +62,8 @@ export default function Todos() {
         <div className='container'>
             <h2>Todo App</h2>
             <form className="input-group mb-3 ">
-                <input type="text" className="form-control text-add" placeholder="What needs to get done?..." aria-label="Recipient's username" aria-describedby="button-addon2" required value={todo} onChange={(e) => setTodo(e.target.value)} />
-                <button className="btn btn-outline-secondary text-add button " type="button" id="button-addon2" onClick={handleAddTodo}>Add Todo</button>
+                <input type="text" className="form-control text-add" placeholder="What needs to get done?..." aria-label="Recipient's username" aria-describedby="button-addon2" required value={todo} onChange={(e) => setTodo(e.target.value)} onKeyDown={handleKeyPress} />
+                <button className="btn btn-outline-secondary text-add button " type="button" id="button-addon2" onClick={handleAddTodo} >Add Todo</button>
             </form>
             {err ? <div className='error'>** enter todo</div> : ''}
             <div className='listItem'>
